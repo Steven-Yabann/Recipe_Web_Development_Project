c49c04936c81dd773ae121bb03ea60ded77ff386
Steven-Yabann <stevekiptoo1@gmail.com>
2024-06-16 12:42:39 +0300

    Added code to display user data from the database to a table

--- phpFiles/test.php
+++ phpFiles/test.php
@@ -1,18 +1,59 @@
 <?php
 require "dbconnection.php";
 
-$sql = "SELECT * FROM categories";
+$sql = "SELECT * FROM users";
 $stmt = $pdo-> prepare($sql);
 $stmt-> execute();
 
-
-if ($stmt->rowCount() > 0){
-    $dataTwo = $stmt-> fetchAll(PDO::FETCH_ASSOC);
-    // foreach($data as $category){
-    //     foreach($category as $name){
-    //         print_r($name);
-    //     }
-    // }
-    print_r($dataTwo);
-}
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DisplayUsers</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+        table, th, td {
+            border: 1px solid black;
+        }
+        th, td {
+            padding: 8px;
+            text-align: left;
+        }
+    </style>
+</head>
+<body>
+<h2>Data from Database</h2>
+
+<table>
+    <tr>
+        <th>Username</th>
+        <th>Email</th>
+        <th>First name</th>
+        <th>Last name</th>
+    </tr>
+    <?php
+    if ($stmt->rowCount() > 0){
+        $data = $stmt-> fetchAll(PDO::FETCH_ASSOC);
+        //print_r($data);
+        foreach($data as $users){
+            echo "<tr>";
+            echo "<td>". $users["username"]. "</td>";
+            echo "<td>". $users["email"]. "</td>";
+            echo "<td>". $users["firstName"]. "</td>";
+            echo "<td>". $users["lastName"]. "</td>";
+            echo "</tr>";
+        }
+    }
+    else {
+        echo "<tr><td colspan='3'>No results found</td></tr>";
+    }
+    ?>
+</table>
+</body>
+</html>
