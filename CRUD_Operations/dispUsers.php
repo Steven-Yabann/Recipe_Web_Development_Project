6bf4a07d47baec73b89178c621115aed85e33504
Steven-Yabann <stevekiptoo1@gmail.com>
2024-07-01 21:31:47 +0300

    Styled the users table and added a button to the edit users page where one can edit their user details

--- CRUD_Operations/dispUsers.php
+++ CRUD_Operations/dispUsers.php
@@ -4,47 +4,40 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DisplayUsers</title>
-    <style>
-        table {
-            width: 100%;
-            border-collapse: collapse;
-        }
-        table, th, td {
-            border: 1px solid black;
-        }
-        th, td {
-            padding: 8px;
-            text-align: left;
-        }
-    </style>
+    <link rel="stylesheet" href="..\cssSheets\tableStyle.css">
 </head>
 <body>
-    <h2>Data from Database</h2>
-    <table>
-    <tr>
-        <th>Username</th>
-        <th>Email</th>
-        <th>First name</th>
-        <th>Last name</th>
-    </tr>
-    <?php
-    require 'userConn.php';
-    if ($stmt->rowCount() > 0){
-        $data = $stmt-> fetchAll(PDO::FETCH_ASSOC);
-        //print_r($data);
-        foreach($data as $users){
-            echo "<tr>";
-            echo "<td>". $users["username"]. "</td>";
-            echo "<td>". $users["email"]. "</td>";
-            echo "<td>". $users["firstName"]. "</td>";
-            echo "<td>". $users["lastName"]. "</td>";
-            echo "</tr>";
+    <div id="mainDiv"> 
+        <h2>Data from Database</h2>
+        <table>
+            <tr id="header">
+                <th>Username</th>
+                <th>Email</th>
+                <th>First name</th>
+                <th>Last name</th>
+            </tr>
+            <?php
+        require 'userConn.php';
+        if ($stmt->rowCount() > 0){
+            $data = $stmt-> fetchAll(PDO::FETCH_ASSOC);
+            //print_r($data);
+            foreach($data as $users){
+                echo "<tr id='data'>";
+                echo "<td>". $users["username"]. "</td>";
+                echo "<td>". $users["email"]. "</td>";
+                echo "<td>". $users["firstName"]. "</td>";
+                echo "<td>". $users["lastName"]. "</td>";
+                echo "</tr>";
+            }
         }
-    }
-    else {
-        echo "<tr><td colspan='3'>No results found</td></tr>";
-    }
-    ?>
-    </table>
+        else {
+            echo "<tr><td colspan='3'>No results found</td></tr>";
+        }
+        ?>
+        </table>
+        <a href="editUsers.php">
+            <button>Edit Profiles</button>
+        </a>
+    </div>
 </body>
 </html>
