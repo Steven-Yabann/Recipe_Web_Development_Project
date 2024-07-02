78b84bbd629fc725e65f3391f171b6ac00070779
Steven-Yabann <stevekiptoo1@gmail.com>
2024-07-02 10:30:17 +0300

    changed the style link for the page

--- CRUD_Operations/editUsers.php
+++ CRUD_Operations/editUsers.php
@@ -18,7 +18,7 @@ $users = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="..\cssSheets\loginPageStyle.css">
+    <link rel="stylesheet" href="..\cssSheets\formDetails.css">
     <title>Edit User</title>
 </head>
 <body>
